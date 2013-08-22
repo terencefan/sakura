994c892fccb15c9f45e8c501be13abcdd2eb1a71
ls <stdrickforce@gmail.com>
2013-08-22 15:40:46 +0800

    add setuptools

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+setup(
+    name="sakura",
+    version="1.0",
+    description="stdrickforce's ubuntu rapid configuration solution",
+    long_description=open("README.md").read(),
+    author="[STD]Rick_Force",
+    author_email="stdrickforce@gmail.com",
+    packages=find_packages(),
+    pacakage_data={},
+)
