4e7141eaacd96203f48475f8f3e1a7d99bfe9415
ls <stdrickforce@gmail.com>
2013-08-28 14:51:42 +0800

    some fab commands

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,41 @@
 # -*- coding: utf-8 -*-
 
-from fabric.api import env, run
+import datetime
+import os
 
-env.hosts = ["localhost"]
+from fabric.api import cd, local
 
-def host_type():
-    run('uname -s')
+FILE_NOT_FOUND = u'文件未找到: {}'
+
+def backup(file_path, exc=False):
+    if os.path.exists(file_path):
+        local('sudo cp {} {}.{}'.format(
+            file_path, file_path,
+            datetime.datetime.now().strftime('%y%m%d%H%M%S'),
+        ))
+        return True
+    elif exc:
+        raise Exception(FILE_NOT_FOUND.format(file_path))
+    return False
+
+def source():
+    a = local('uname -r')
+    backup('/etc/apt/sources.list')
+
+def upgrade():
+    local('sudo apt-get upgrade')
+
+def install():
+    require_apts = ['git', 'ssh', 'vim', 'vim-gnome', 'ncurses-base']
+    for apt in require_apts:
+        local('sudo apt-get install {}'.format(apt))
+
+def bash():
+    file_path = 'sakura/bash/'
+    files = ['bashrc', 'bash_aliases', 'bash_prompt']
+    for f in files:
+        local('sudo cp {}{} ~/.{}'.format(file_path, f, f))
+    local('source ~/.bashrc')
+
+def fast():
+    bash()
--- sakura/source/source.sh
+++
@@ -1,5 +0,0 @@
-if [ -f "/etc/apt/sources.list.backup" ];then
-    sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
-fi
-sudo cp sources-13.04.list /etc/apt/sources.list
-# sudo apt-get update
