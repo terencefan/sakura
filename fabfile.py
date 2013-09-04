166bd4c492fe36f4c77d2f66b940de1e5ce1ac21
stdrickforce <stdrickforce@gmail.com>
2013-09-04 19:27:04 +0800

    update

--- fabfile.py
+++ fabfile.py
@@ -24,6 +24,7 @@ VERSION_NAME = {
 # env.hosts = ['elemedev@192.168.108.120']
 env.hosts = []
 
+
 def backup(file_path, exc=False):
     if os.path.exists(file_path):
         local('cp {} {}.{}'.format(
