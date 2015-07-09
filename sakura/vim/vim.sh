565f00691ffbe9d63ebf69c31329d373ffe79e4d
stdrickforce <stdrickforce@gmail.com>
2015-07-09 17:42:31 +0800

    fix vim

--- sakura/vim/vim.sh
+++ sakura/vim/vim.sh
@@ -6,6 +6,3 @@ git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
 git clone https://github.com/rainly/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 git clone https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/vim-colorschemes
-
-sudo apt-get install vim
-sudo apt-get install vim-gnome
